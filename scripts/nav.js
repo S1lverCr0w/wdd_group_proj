7f5e7d6ecba1515e383a3155e4602392ff57c5c6
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-11-22 19:15:25 +0000

    brought other member pages and using one navbar and footer script

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,16 +1,9 @@
-// fetch('nav.html')
-// .then(res => res.text())
-// .then(text => {
-// 	let oldelem = document.querySelector("script#replace_with_navbar");
-// 	let newelem = document.createElement("div");
-// 	newelem.innerHTML = text;
-// 	oldelem.parentNode.replaceChild(newelem,oldelem);
-// })
-
+// all worked on navbar development
+// Tristan, Alex, Derek, Andrei
 var navbar = `
 		<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
 			<div class="container-fluid">
-				<a class="navbar-brand"  href="index.html">Logo: Company</a>
+				<a class="navbar-brand"  href="index.html">Group G</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
@@ -20,9 +13,9 @@ var navbar = `
 					<a class="nav-link navBtn"  href="news.html">News</a>
 					<a class="nav-link navBtn"  href="uisceire.html">UisceIRE</a>
 					<a class="nav-link navBtn"  href="sources.html">Sources</a>
-					<a class="nav-link navBtn"  href="conserv.html">Conservation</a>
+					<a class="nav-link navBtn"  href="conservation.html">Conservation</a>
 					<a class="nav-link navBtn"  href="findus.html">Find Us</a>
-					<a class="nav-link navBtn"  href="contactus.html">Contact Us</a>
+					<a class="nav-link navBtn"  href="contact.html">Contact Us</a>
 					<a class="nav-link navBtn"  href="info.html">Info</a>
 				</div>
 			</div>
