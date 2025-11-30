5fbff7e027a2e194f0ad33f6ffc211de5da5233c
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-11-30 21:49:18 +0000

    javascript login v04 registration logic in progress

--- scripts/ag_validation.js
+++ scripts/ag_validation.js
@@ -51,11 +51,86 @@ function validate_form() {
 	const username = document.getElementById("ag_username").value;
 	const fname = document.getElementById("ag_fname").value;
 	const lname = document.getElementById("ag_lname").value;
+	const occupation = document.getElementById("ag_occupation").value;
 	const email = document.getElementById("ag_email").value;
 	const date = document.getElementById("ag_date").value;
 	const passwd = document.getElementById("ag_passwd").value;
 	const rpasswd = document.getElementById("ag_rpasswd").value;
-	const agree = document.getElementById("ag_agree").value;
+	const agree = document.getElementById("ag_agree").checked;
+
+	const username_error = document.getElementById("username_error");
+	const fname_error = document.getElementById("fname_error");
+	const lname_error = document.getElementById("lname_error");
+	const occupation_error = document.getElementById("occupation_error");
+	const email_error = document.getElementById("email_error");
+	const date_error = document.getElementById("date_error");
+	const passwd_error = document.getElementById("passwd_error");
+	const rpasswd_error = document.getElementById("rpasswd_error");
+	const agree_error = document.getElementById("agree_error");
+
+	username_error.textContent = "";
+	fname_error.textContent = "";
+	lname_error.textContent = "";
+	occupation_error.textContent = "";
+	email_error.textContent = "";
+	date_error.textContent = "";
+	passwd_error.textContent = "";
+	rpasswd_error.textContent = "";
+	agree_error.textContent = "";
+
+	let form_validity = true;
+
+	if (username == "" || username.length < 3) {
+		username_error.textContent = "Please enter a valid username.";
+		form_validity = false;
+	}
+
+	if (fname == "" || fname.length < 3) {
+		fname_error.textContent = "Please enter a valid name.";
+		form_validity = false;
+	}
+
+	if (lname == "" || lname.length < 3) {
+		occupation_error.textContent = "Please enter a valid surname.";
+		form_validity = false;
+	}
+
+	if (occupation == "") {
+		occupation_error.textContent = "Please select your occupation.";
+		form_validity = false;
+	}
+
+	if (!email.includes("@") || !email.includes(".")) {
+		email_error.textContent = "Please enter a valid email address.";
+		form_validity = false;
+	}
+
+	if (date == "") {
+		date_error.textContent = "Please choose a valid date of birth.";
+		form_validity = false;
+	}
+
+	if (passwd == "" || passwd.length < 6) {
+		passwd_error.textContent = "Please enter a valid password.";
+		form_validity = false;
+	}
+
+	if (rpasswd == "" || rpasswd.length < 6) {
+		rpasswd_error.textContent = "Passwords don't match.";
+		form_validity = false;
+	}
+
+	if (!agree) {
+		agree_error.textContent = "Please agree to the terms and conditions.";
+		form_validity = false;
+	}
+
+	if (form_validity) {
+		alert("Your account has been created! \n\nYou can now log in with your username and password.")
+		return true;
+	} else {
+		return false;
+	}
 }
 
 
