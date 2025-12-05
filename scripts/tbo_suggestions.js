4166a26a38a0662e814686c2b4a40747c73bebcb
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-12-05 22:48:58 +0000

    final merge

--- scripts/tbo_suggestions.js
+++ scripts/tbo_suggestions.js
@@ -1,17 +1,19 @@
 // Tristan's form validation
 
-
+//when validForm() is called , it will validate each constant
 function validForm() {
-	const names = document.getElementById("names").value;              //
+	const names = document.getElementById("names").value;              
 	const surname = document.getElementById("surname").value;
 	const email = document.getElementById("email").value;
 	const suggest = document.getElementById("suggest").value;
 	
+	// this is used for detect any errors
 	const name_Err = document.getElementById("names_error");
 	const surn_Err = document.getElementById("surname_error");
 	const email_Err = document.getElementById("email_error");
 	const sug_Err = document.getElementById("sug_error");
 	
+	//this is used to make sure the field is not left empty
 	name_Err.textContent = "";
 	surn_Err.textContent = "";
 	email_Err.textContent = "";
@@ -19,26 +21,31 @@ function validForm() {
 
 	var is_form_valid = true;
 	
+	//length check for names box
 	if(names === "" || names.length < 2){
 		name_Err.textContent = "Please eneter your name correctly";
 		is_form_valid = false;
 	}
 	
+	//length check for surname box
 	if(surname === "" || surname.length < 2){
 		surn_Err.textContent = "Please eneter your surname correctly";
 		is_form_valid = false;
 	}
 	
-	if(email === "" || email.length < 2){
+	//length check for email box
+	if(email === "" || email.length < 5){
 		email_Err.textContent = "Please eneter a valid Email";
 		is_form_valid = false;
 	}
 	
-	if(suggest === "" || suggest.length < 2){
+	//length check for suggestion box
+	if(suggest === "" || suggest.length < 10){
 		sug_Err.textContent = "Please eneter a suggestion";
 		is_form_valid = false;
 	}
 	
+	//if the is_form_valid remains true , they alert is displayed
 	if(is_form_valid){
 		alert("Form submitted successfuly");
 		return true;
